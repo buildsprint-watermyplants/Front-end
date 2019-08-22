05debddeec843823b109f379f79a511a6481614d
Kyle Barnett <kbarnett23@gmail.com>
2019-08-22 10:28:19 -0500

    got display for create profile form

--- watermyplants/src/components/CreateProfile/CreateProfile.js
+++ watermyplants/src/components/CreateProfile/CreateProfile.js
@@ -1,9 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { withFormik, Form, Field } from 'formik';
+import axios from 'axios';
+import * as Yup from "yup";
+import './CreateProfile.css';
 
-export default function CreateProfile() {
+const CreateProfile = ({ errors, touched, values, status }) => {
+  const [users, setUsers] = useState([])
   return (
-    <div>
-      Create Profile
+    <div className="user-form-container">
+      <header>
+        <h1>Register to Monitor Your Plants!</h1>
+      </header>
+      <Form>
+        <Field
+          className="input-fields"
+          component="input"
+          type="text"
+          name="username"
+          placeholder="Username"
+        />
+        <Field
+          className="input-fields"
+          component="input"
+          type="password"
+          name="password"
+          placeholder="Password"
+        />
+        <Field
+          className="input-fields"
+          component="input"
+          type="text"
+          name="phone"
+          placeholder="Phone Number"
+        />
+        <button>Create Account!</button>
+      </Form>
     </div>
   )
 }
+
+const formikHOC = withFormik({
+  mapPropsToValues({ username, password, phone }) {
+    return {
+      username: username || "",
+      password: password || "",
+      phone: phone || ""
+    }
+  },
+  validationSchema: Yup.object().shape({
+    username: Yup.string().required('Not a good username!'),
+    password: Yup.string().min(8).required('Must be longer than 8 characters!')
+  })
+})
+
+const FormFieldWithFormik = formikHOC(CreateProfile)
+
+export default FormFieldWithFormik;
