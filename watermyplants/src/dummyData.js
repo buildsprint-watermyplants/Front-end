5fca27fef54209566a92c808ce273514b1f87404
cecil john tantay <cjbtantay@gmail.com>
2019-08-22 19:44:57 -0700

    add dummy data

---
+++ watermyplants/src/dummyData.js
@@ -0,0 +1,28 @@
+const data = {
+  "id": 1,
+  "username": "admin",
+  "password": "password",
+  "phoneNumber": "5555555555",
+  "plants": [
+      {
+          "id": 1,
+          "plantName": "Watermelon",
+          "dailyWaterTime": "08:00:00",
+          "user_id": 1
+      },
+      {
+          "id": 2,
+          "plantName": "Tomato",
+          "dailyWaterTime": "09:00:00",
+          "user_id": 1
+      },
+      {
+          "id": 3,
+          "plantName": "Sunflower",
+          "dailyWaterTime": "10:00:00",
+          "user_id": 1
+      }
+  ]
+}
+
+export default data;
