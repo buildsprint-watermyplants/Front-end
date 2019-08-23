385d3342e0337f7d6ffa823f9320d902607bde0f
Kyle Barnett <kbarnett23@gmail.com>
2019-08-22 22:33:32 -0500

    added error messages to createprofile

--- watermyplants/src/App.js
+++ watermyplants/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Routes from './components/Routes/Routes';
+import Dashboard from './components/Dashboard/Dashboard';
 
 function App() {
   return (
     <div className="App">
       <Routes />
+      <Dashboard />
     </div>
   );
 }
--- watermyplants/src/components/Dashboard/Dashboard.js
+++ watermyplants/src/components/Dashboard/Dashboard.js
@@ -1,16 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import data from '../../dummyData';
 
 export default function Dashboard() {
 
-  const [plants, setPlants] = useState([])
-  useEffect(() => {
-    axios
-      .get('https://bs-water-my-plants.herokuapp.com/')
-      .then(res => console.log(res))
-  })
+  // const [plants, setPlants] = useState([])
   return (
     <div>
+      <p>{data.plants[0].plantName}</p>
     </div>
   )
 }
