a34042dfa89139b406616cba638edb9d8a37c8f9
Kyle Barnett <kbarnett23@gmail.com>
2019-08-26 23:18:33 -0400

    deleted some unecessary mapping displays

--- watermyplants/src/components/Plants/CreatePlants.js
+++ watermyplants/src/components/Plants/CreatePlants.js
@@ -39,9 +39,6 @@ const CreateProfile = ({ errors, touched, values, status }) => {
         />
         <button>Add Plant!</button>
       </Form>
-      {plants.map(plant => (
-        <p key={plant.id}>{plant.plantName}</p>
-      ))}
     </div>
   )
 }
