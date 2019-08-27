f67d07f83e3260b37d6e11f074e80f78fdac188f
LiamColeE <liamedlinger@gmail.com>
2019-08-26 19:34:59 -0700

    fixed plant display

--- watermyplants/src/components/PlantDisplay/PlantDisplay.js
+++ watermyplants/src/components/PlantDisplay/PlantDisplay.js
@@ -2,14 +2,42 @@ import React, { useState, useEffect } from 'react';
 import PlantCard from './PlantCard'
 import { Card } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
+import axios from 'axios';
 
 export default function PlantDisplay(props) {
+    const [plants, setPlants] = useState();
+
+    useEffect(() => {
+        axios.get(`/api/plants/`, {
+          headers: {
+            contentType: "application/JSON",
+            authorization: localStorage.token
+          }
+        })
+          .then(function (res) {
+            console.log(res);
+            setPlants(res.data);
+          })
+          .catch(function (error) {
+            console.log(error);
+          })
+      }, [])
+
+    if(!plants){
+        return(
+            <div>
+                <h1>
+                    Loading Plants...
+                </h1>
+            </div>
+        )
+    }
     return (
         <div>
             <hr />
             <Card.Group centered itemsPerRow={3} stackable>
                 {
-                    props.plants.map(element => <PlantCard plant={element} />)
+                    plants.map(element => <PlantCard plant={element} />)
                 }
             </Card.Group>
         </div>
